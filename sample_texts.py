02ce416aa6aa75a42c7a773b18f0922dc84adc52
Dennis Nguyen-Do <dennisnguyendo@gmail.com>
2020-04-25 19:35:34 -0400

    refactor constants

---
+++ sample_texts.py
@@ -0,0 +1,49 @@
+SAMPLES_TEXT_1 = [
+    ["Hi there"],
+    ["How can I help you?"],
+    ["Are there any flights from Toronto to San Diego"],
+    ["Yes, for which dates would you like?"],
+    ["June 3rd to 10th"],
+    ["How much would like to spend?"],
+    ["Under 500"],
+]
+
+SAMPLES_TEXT_2 = [
+    ["Good Morning"],
+    ["How can i help you?"],
+    ["I would like to see if I can travel from Seattle to Chicago tomorrow"],
+    ["When would you like to come back"],
+    ["In 5 weeks, but I am flexible on the day"],
+    ["What is your budget"],
+    ["Less than fifteen hundred dollars"],
+]
+
+SAMPLES_TEXT_3 = [
+    ["Yo"],
+    ["How can I help you?"],
+    [
+        "How much is it to go from San Diego to Los Angeles. I have to get there for a wedding."
+    ],
+    ["When would you like to travel"],
+    ["March 1st to March 2nd"],
+    ["What is your budget"],
+    ["The cheapest you can find"],
+]
+
+SAMPLES_TEXT_4 = [
+    ["Hello"],
+    ["How can I help you?"],
+    ["I would like to fly to San Francisco on the 5th of December"],
+    ["What is your budget"],
+    ["Price is no object, and I would like first class if possible"],
+]
+
+SAMPLES_TEXT_5 = [
+    ["Hello"],
+    ["How can I help you?"],
+    ["I would like to fly to Portland as soon as possible"],
+    ["Where are you located"],
+    ["Washington"],
+    ["What is your budget"],
+    ["Between $400 and $500"],
+]
